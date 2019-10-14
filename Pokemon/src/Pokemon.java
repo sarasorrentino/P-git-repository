5cd4637ed01d676d8b0be0e489659f36fc1a542e
sarasorrentino <56540290+sarasorrentino@users.noreply.github.com>
2019-10-14 12:04:06 +0200

    Add files via upload

---
+++ Pokemon/src/Pokemon.java
@@ -0,0 +1,15 @@
+
+public class Pokemon {
+
+	protected String nome;
+	protected Tipo tipo;
+	protected Mossa[] mossa;
+	mosse = new Mossa[4];
+	
+	public Pokemon (String nome, Tipo tipo, Mossa[] mosse){
+		this.nome=nome;
+		this.tipo=tipo;
+		this.mossa=mosse;
+	}
+	
+}
